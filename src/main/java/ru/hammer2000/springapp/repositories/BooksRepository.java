74818758797554f88016d3997ac2be04967cfe62
hammer2000 <hammer2k2@gmail.com>
2023-05-31 18:08:42 +0300

    Initial commit

---
+++ src/main/java/ru/hammer2000/springapp/repositories/BooksRepository.java
@@ -0,0 +1,12 @@
+package ru.hammer2000.springapp.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hammer2000.springapp.model.Book;
+import ru.hammer2000.springapp.model.Person;
+
+import java.util.List;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWithIgnoreCase(String title);
+}
